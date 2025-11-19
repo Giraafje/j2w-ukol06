5de0581c6d17601ddd2d47531cd171e0673db41b
Lenka Erbenova <lenka.erbenova79@gmail.com>
2025-11-19 19:55:35 +0100

    Add setters to Java Bean "Vizitka" -> form is working as expected, new Business Card is added to the database

--- src/main/java/cz/czechitas/java2webapps/ukol6/entity/Vizitka.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/entity/Vizitka.java
@@ -47,7 +47,8 @@ public class Vizitka {
   private String web;
 
   //class constructor
-  public Vizitka() {}
+  public Vizitka() {
+  }
 
   public Vizitka(Integer id, String celeJmeno, String firma, String ulice, String obec, String psc, String email, String telefon, String web) {
     this.id = id;
@@ -73,36 +74,68 @@ public class Vizitka {
     return celeJmeno;
   }
 
+  public void setCeleJmeno(String celeJmeno) {
+    this.celeJmeno = celeJmeno;
+  }
+
   public String getFirma() {
     return firma;
+  }
+
+  public void setFirma(String firma) {
+    this.firma = firma;
   }
 
   public String getUlice() {
     return ulice;
   }
 
+  public void setUlice(String ulice) {
+    this.ulice = ulice;
+  }
+
   public String getObec() {
     return obec;
+  }
+
+  public void setObec(String obec) {
+    this.obec = obec;
   }
 
   public String getEmail() {
     return email;
   }
 
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
   public String getTelefon() {
     return telefon;
+  }
+
+  public void setTelefon(String telefon) {
+    this.telefon = telefon;
   }
 
   public String getWeb() {
     return web;
   }
 
+  public void setWeb(String web) {
+    this.web = web;
+  }
+
   public String getPsc() {
     return psc;
   }
 
+  public void setPsc(String psc) {
+    this.psc = psc;
+  }
+
   public String getCelaAdresa() {
-    return ulice + ", " + psc + " " +  obec;
+    return ulice + ", " + psc + " " + obec;
   }
 
 }
